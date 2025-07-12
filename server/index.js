e27b725d22fc5f98f8da5658af5c08a14142750e
jaadu611 <ayushburman281@gmail.com>
2025-07-12 23:18:41 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -28,10 +28,11 @@ app.get("/", (req, res) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/dist")));
+  const clientBuildPath = path.join(__dirname, "../client/dist");
+  app.use(express.static(clientBuildPath));
 
-  app.get(/.*/, (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(clientBuildPath, "index.html"));
   });
 }
 
