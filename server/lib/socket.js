7b96f0200177a3d528d962e5ac977738a1637a98
jaadu611 <ayushburman281@gmail.com>
2025-07-12 22:21:49 +0530

    Update socket.js

--- server/lib/socket.js
+++ server/lib/socket.js
@@ -3,7 +3,7 @@ const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
 
-const app = express();
+const app = express.Router();
 const server = http.createServer(app);
 
 const userSocketMap = {};
